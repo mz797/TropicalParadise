55f69b664cd78281ccbbf8b6a2527d2dec6108e3
Magda <magda.79720@wp.pl>
2022-08-19 21:45:44 +0200

    finished site

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 const nav = document.querySelector('.nav__items');
 const allNavItems = document.querySelectorAll('.nav__item');
 const navBtn = document.querySelector('.burger-btn');
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
+
 
 const handleNav=() =>{
     nav.classList.toggle('nav__items--active')
+
+    navBtnBars.classList.remove('black-bars-colors') //usuwa ciemny kolor z btn jeśli nawigacja jest włączona
+
     allNavItems.forEach(item =>{
         item.addEventListener('click',()=>{
             nav.classList.remove('nav__items--active')
@@ -11,6 +18,8 @@ const handleNav=() =>{
     })
     handleNavItemsAnimation();
 }
+
+
 const handleNavItemsAnimation = () =>{
     let delayTime = 0;
     allNavItems.forEach(item => {
@@ -19,4 +28,29 @@ const handleNavItemsAnimation = () =>{
         delayTime++;
     })
 }
+
+const handleObserver =()=>{
+    const currentSection = window.scrollY;
+    allSections.forEach(section=>{
+        if(section.classList.contains('white-section') && section.offsetTop <= currentSection+60){
+            navBtnBars.classList.add('black-bars-colors')
+        }
+        else if(!section.classList.contains('white-section') && section.offsetTop <= currentSection+60){
+            navBtnBars.classList.remove('black-bars-colors')
+        }
+
+
+    })
+}
+
+
+
+const handleCurrentYear = () =>{
+    const year = new Date().getFullYear();
+    footerYear.innerHTML = year;
+}
+
+window.addEventListener('scroll',handleObserver)
+handleCurrentYear();
+
 navBtn.addEventListener('click',handleNav);
